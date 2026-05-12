639acba50b550856edc4535acfc1f8b8eea48596
vkptimon <venkatakoushikp2002@gmail.com>
2026-05-12 08:36:33 +0530

    day 3, pushing the empty file to work on later

---
+++ InsertInterval.java
@@ -0,0 +1,9 @@
+public class InsertInterval {
+    public static void main(String[] args) {
+        
+    }
+
+    public static int[][] insert(int[][] intervals, int[] newInterval){
+        
+    }
+}
