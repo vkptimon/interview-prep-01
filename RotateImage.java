f529af606cc637c18b4b82a154daddc79e0c5e09
vkptimon <venkatakoushikp2002@gmail.com>
2026-05-22 12:00:44 +0530

    day 3, just adding the skeletal file and rough algo

---
+++ RotateImage.java
@@ -0,0 +1,7 @@
+public class RotateImage {
+    public static void main(String[] args) {
+        /** Rough Algorithm
+         * We can loop through each entry and add the 1st, 2nd.... jth element into the corresponding list at jth position
+         */
+    }    
+}
